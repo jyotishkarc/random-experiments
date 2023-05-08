128ae0a5d533474af1d5e8ea3db925de9bdc3a28
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2023-05-08 10:58:44 +0530

    wrote some more instructions

--- exmpl.bat
+++ exmpl.bat
@@ -1,4 +1,14 @@
 set R_Script="C:\Program Files\R\R-4.2.2\bin\RScript.exe"
 
-%R_Script% exmpl.R 5 6 -3 > exmpl.batch 2>&1
-%R_Script% exmpl.R h 2 100 > exmpl-abcd.batch 2>&1
+%R_Script% exmpl.R 5 6 -3 > exmpl-1.batch 2>&1
+%R_Script% exmpl.R h 2 100 > exmpl-2.batch 2>&1
+%R_Script% exmpl.R 3 2 100 > exmpl-3.batch 2>&1
+%R_Script% exmpl.R -34 21 112 > exmpl-4.batch 2>&1
+
+
+
+
+
+
+
+
