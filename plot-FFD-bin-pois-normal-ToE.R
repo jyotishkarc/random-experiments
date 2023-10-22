984ffd6e4de70b460ef82bfd2153c6b286d42cf9
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2023-10-23 00:16:57 +0530

    spacrt project

---
+++ plot-FFD-bin-pois-normal-ToE.R
@@ -0,0 +1,118 @@
+
+library(simulatr)
+library(dplyr)
+library(ggplot2)
+library(gridExtra)
+library(gtable)
+library(grid)
+
+#####################
+results_rds <- readRDS("~/R/R Codes/Experiments with R/temp-spacrt-result/grid_explore_FFD_bin_pois_normal_B_5000_5e3_n5_n5_w_score_results.rds")
+#####################
+
+#####################
+(n_methods <- results_rds$results %>% dplyr::pull(method) %>% unique() %>% length())
+(n_levels <- results_rds$metrics %>% dplyr::pull(metric) %>% unique() %>% length() - 3)
+#####################
+
+#####################
+# 1. Parameter grid
+#####################
+varying_params <- list(n = c(1e3,5e3,1e4,5e4,1e5),
+                       gamma_0 = seq.int(from = -6, to = -2),
+                       beta_0 = seq.int(from = -6, to = -2))
+
+baseline_params <- list(n = 5e3, gamma_0 = -5, beta_0 = -5)
+
+(grid_ffd_orig <- create_param_grid_fractional_factorial(varying_params,baseline_params))
+
+(grid_ffd <- grid_ffd_orig %>%
+      select(-c(arm_n,arm_gamma_0,arm_beta_0)) %>%
+      # select(-c(grid_id)) %>%
+      `rownames<-`( NULL ) %>%
+      add_row(n = 5e3, gamma_0 = -5, beta_0 = -5, .before = 7) %>%
+      add_row(n = 5e3, gamma_0 = -5, beta_0 = -5, .before = 12))
+
+param_names <- names(baseline_params)
+
+###################
+results_rds_metrics <- results_rds$metrics[1:(n_methods * n_levels * nrow(grid_ffd_orig)), ]
+results_rds_results <- results_rds$results
+###################
+
+plt <- list()
+page.heading <- c()
+
+for(toe in unique(results_rds_metrics$metric)){
+   
+   if(toe == 'toe.new.1') level <- 5e-2
+   if(toe == 'toe.new.2') level <- 1e-2
+   if(toe == 'toe.new.3') level <- 5e-3
+   
+   res.toe <- results_rds_metrics %>% dplyr::filter(metric == toe)
+   unique.metrics <- unique(results_rds_metrics$metric)
+   
+   plt[[which(unique.metrics == toe)]] <- list()
+   
+   for(param in param_names){
+      
+      fixed <- param_names %>% setdiff(param)
+      fixed_pos_1 <- which(fixed[1] == param_names)
+      fixed_pos_2 <- which(fixed[2] == param_names)
+      
+      df <- res.toe %>% dplyr::filter(!!as.symbol(param_names[fixed_pos_1]) == 
+                                         baseline_params[[param_names[fixed_pos_1]]]) %>%
+         dplyr::filter(!!as.symbol(param_names[fixed_pos_2]) == 
+                          baseline_params[[param_names[fixed_pos_2]]]) %>% 
+         dplyr::select(method, mean, se, !!as.symbol(param))
+      
+      plt.df <- df %>% 
+         ggplot(aes(x = !!as.symbol(param),
+                    y = mean,
+                    ymin = mean - 2*se,
+                    ymax = mean + 2*se,
+                    color = method)) +
+         geom_point() +
+         geom_line() +
+         geom_errorbar(width = 0.2) +
+         geom_hline(yintercept = level, linetype = "dashed", color = "red") +
+         ylab("") +
+         scale_y_log10() +
+         {if(param == 'n') scale_x_log10()} +
+         theme_light() +
+         theme(legend.position = "bottom")
+      
+      plt[[which(unique.metrics == toe)]][[which(param_names == param)]] <- plt.df
+      
+      print(which(param_names == param))
+   }
+   
+   plt[[which(unique.metrics == toe)]] <- 
+      gridExtra::grid.arrange(arrangeGrob(
+         plt[[which(unique.metrics == toe)]][[which(param_names == param_names[1])]],
+         plt[[which(unique.metrics == toe)]][[which(param_names == param_names[2])]],
+         plt[[which(unique.metrics == toe)]][[which(param_names == param_names[3])]],
+         left = textGrob("Type-I error (in log_10 scale)", 
+                         rot = 90, vjust = 1),
+         nrow = 1),
+         nrow = 1)
+   
+   print('M')
+   
+   page.heading[which(unique.metrics == toe)] <- 
+      paste0("X|Z ~ Bernoulli, ",
+             "Y|Z ~ Poisson : Level = ",level,"\n")
+}
+
+#################
+
+ggsave(filename = "plot-FFD-bin-pois-normal-5000-5e3-n5-n5-w-score-ToE.pdf", 
+       plot = marrangeGrob(plt, nrow=1, ncol=1,
+                           top = quote(grid::textGrob(page.heading[g], 
+                                                      gp=grid::gpar(fontsize=20,
+                                                                    fontface=2)))),
+       
+       width = 12, height = 7)
+
+
+
---
+++ plot-bin-pois-normal-with-covariates.R
@@ -0,0 +1,100 @@
+
+
+library(dplyr)
+library(ggplot2)
+library(gridExtra)
+library(katlabutils)
+
+parameter_grid_pois <- expand.grid(
+   # n = c(50,seq.int(from = 100, to = 500, by = 100))    # sample size
+   n = seq.int(from = 50, to = 500, by = 50)    # sample size
+   # p = c(3, 5, 8, 12, 15)    # dimension
+   # n = c(20, 30, 40),
+   # p = c(3, 8)    # dimension
+)
+
+
+# bin_pois_normal_B_500000_results <- readRDS("~/R/R Codes/Experiments with R/temp-spacrt-result/bin_pois_normal_B_500000_results.rds")
+
+
+for(k in 2:5){
+   plot.temp <- bin_pois_normal_B_500000_results$metrics[1:120, ] |>
+      dplyr::filter(n != 50) |>
+      dplyr::filter(metric == paste0('toe',k)) |>
+      ggplot(aes(x = n,
+                 y = mean,
+                 ymin = mean - 2*se,
+                 ymax = mean + 2*se,
+                 color = method)) +
+      geom_point() +
+      geom_line() +
+      geom_errorbar(width = 1) +
+      geom_hline(yintercept = 10^(-k), linetype="dashed", color = "red") +
+      labs(x = "Sample size",
+           y = "Type-I error") +
+      theme_light() +
+      theme(legend.position = "bottom")
+   
+   assign(paste0('plot.pois.toe.',k), plot.temp)
+   print(k)
+}
+
+
+ggpubr::ggarrange(plot.pois.toe.2,
+                  plot.pois.toe.3,
+                  plot.pois.toe.4,
+                  plot.pois.toe.5,
+                  common.legend = T,
+                  legend = 'bottom')
+
+# ggsave('plot-bin-pois-normal-with-covariates-toe.pdf')
+
+df.pvals <- df.qq <- df.qq.proper <- qq_plots_pois <- list()
+
+for(i in sort(unique(bin_pois_normal_B_500000_results$results$grid_id))){
+   
+   df.qq[[i]] <- bin_pois_normal_B_500000_results$results %>%
+      # dplyr::select(run_id) %>%
+      dplyr::filter(grid_id == i) %>%
+      dplyr::select(-grid_id) #%>%
+      # dplyr::filter(output != 0)
+   
+   df.qq.proper[[i]] <- data.frame(method = df.qq[[i]]$method %>% as.character(), 
+                              run_id = df.qq[[i]]$run_id %>% as.integer(),
+                              p.value = df.qq[[i]]$output %>% unlist()) %>% arrange(run_id)
+   
+   # reshape2::melt(id =  value.name = 'p-value') %>%
+   
+   chosen <- sample.int(500000, 20000)
+   
+   (qq_plots_pois[[i]] <- df.qq.proper[[i]] %>%
+      filter(run_id %in% chosen) %>%
+      # qq_plots[[i]] <- df.slice[[i]] %>%
+      ggplot(aes(y = p.value, color = method)) +
+      # geom_point() +
+      stat_qq_points(max_pts_to_plot = 20000) +
+      stat_qq_band() +
+      geom_abline() +
+      # scale_x_log10() +
+      # scale_x_reverse(trans = revlog_trans()) +
+      scale_x_continuous(trans = revlog_trans(),
+                         breaks = c(1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6)) +
+      scale_y_continuous(trans = revlog_trans(),
+                         breaks = c(1, 1e-2, 1e-4, 1e-6, 1e-8, 1e-10)) +
+      theme_light() +
+      ggtitle(paste0("n = ",parameter_grid_pois$n[i])) +
+      theme(legend.position = "bottom", 
+            legend.title = element_blank(),
+            plot.title = element_text(hjust = 0.5, face = 'bold')) +
+      labs(x = "Expected null p-value",
+           y = "Observed p-value"))
+   
+   print(i)
+}
+
+
+ggsave(filename = "plot-bin-pois-normal-covariate-qqplots.pdf", 
+       plot = marrangeGrob(qq_plots_pois, nrow=1, ncol=2),
+       width = 12, height = 7)
+
+
